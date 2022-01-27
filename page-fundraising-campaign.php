81ebe1375c35bed16ba744aee468458782749f40
Piotr Markielau <pmarkielau@gmail.com>
2022-01-27 22:39:23 +0200

    feat: add page-fundraising-campaign.php
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

---
+++ page-fundraising-campaign.php
@@ -0,0 +1,27 @@
+<?php /* Template Name: page-fundraising */ ?>
+
+<?php get_header(); ?>
+
+<div class="container bg-light px-0 pb-4 pt-4 cont">
+    <div class="jumbotron d-flex flex-column jumbotron-fluid justify-content-center align-items-center"
+         style="background-image: linear-gradient(135deg, rgba(229, 27, 77, 0.5), rgba(62, 2, 166, 0.5)),
+                 url(<?php bloginfo('template_url'); ?>/images/forestaction.jpg);">
+        <p class="display-4 text-light text-shadow"><?php the_title(); ?></p>
+    </div>
+</div>
+
+<div class="container cont bg-light pb-5 px-md-5">
+
+    <main role="main">
+        <section>
+
+
+
+        </section>
+    </main>
+
+
+</div>
+
+
+<?php get_footer(); ?>
