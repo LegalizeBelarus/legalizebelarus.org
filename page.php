66fea10c76379c5bf684e5feb4002e46f8d4acd9
Piotr Markielau <pmarkielau@gmail.com>
2022-08-21 11:50:14 +0200

    Add $('[data-toggle="tooltip"]').tooltip() to pages
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- page.php
+++ page.php
@@ -1,19 +1,26 @@
 <?php get_header(); ?>
 
-    <div class="container bg-light px-0 pt-4 cont">
+<div class="container bg-light px-0 pt-4 cont">
 
-        <div class="container pb-3 text-center">
-            <p class="display-4"><?php the_title(); ?></p>
-        </div>
-
-        <div class="container pb-4 px-5">
-            <p class="lead">
-                <?php the_post();
-                the_content(); ?>
-            </p>
-        </div>
-
+    <div class="container pb-3 text-center">
+        <p class="display-4"><?php the_title(); ?></p>
     </div>
+
+    <div class="container pb-4 px-5">
+        <p class="lead">
+            <?php the_post();
+            the_content(); ?>
+        </p>
+    </div>
+
+</div>
 
 
 <?php get_footer(); ?>
+
+<script>
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
+
