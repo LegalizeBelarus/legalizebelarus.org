fca7af8948b31a2e15aac96ce4f117fc6dce6790
Piotr Markielau <pmarkielau@gmail.com>
2023-08-04 20:00:32 +0200

    Дадаць Яўгена
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- page-pra-nas.php
+++ page-pra-nas.php
@@ -243,12 +243,12 @@
 
 
                 <div class="col-6 col-md-4 col-lg-3 mb-3">
-                    <a target="_blank" class="no-dec text-dark" href="https://facebook.com/yan.yakimovich">
+                    <a target="_blank" class="no-dec text-dark" href="https://t.me/yauhen">
                         <div class="card">
-                            <img src="<?php bloginfo( 'template_url' ); ?>/images/team/Jan.jpg" class="card-img-top">
+                            <img src="<?php bloginfo( 'template_url' ); ?>/images/team/Jauhen.jpg" class="card-img-top">
                             <div class="card-body rad-b">
-                                <h5 class="card-title">Ян Якимович</h5>
-                                <p class="card-text font-weight-normal">Медиа-служба</p>
+                                <h5 class="card-title">Евгений Гучок</h5>
+                                <p class="card-text font-weight-normal">Технический директор</p>
                             </div>
                         </div>
                     </a>
