cca3d4faa7814a7050df48999328eff101b39418
Piotr Markielau <pmarkielau@gmail.com>
2024-02-17 21:59:39 +0100

    Update single.php
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- single.php
+++ single.php
@@ -13,7 +13,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-9">
-                        <div class="lead-bold mb-4">
+                        <div class="container lead-bold mb-4">
                             <?php if ( ! has_excerpt() ) {
                                 echo '';
                             } else {
