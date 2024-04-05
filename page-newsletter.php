2fd0b490c66174bb3411dca382167617fed376c0
Piotr Markielau <pmarkielau@gmail.com>
2024-04-05 19:28:44 +0200

    Add newsletter page
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- page-newsletter.php
+++ page-newsletter.php
@@ -3,7 +3,7 @@
 <?php get_header(); ?>
 
 
-<div class="container bg-light px-0 pt-4 cont">
+<div class="container bg-light px-0 pt-4 mb-0 cont">
     <div class="jumbotron d-flex flex-column jumbotron-fluid justify-content-center align-items-center px-3 text-center"
          style="background-image: linear-gradient(135deg, rgba(229, 27, 77, 0.5), rgba(62, 2, 166, 0.5)),
                  url(<?php bloginfo( 'template_url' ); ?>/images/newsletter.jpg);">
