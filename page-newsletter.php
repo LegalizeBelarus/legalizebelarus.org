b8cafdc5b7181c0e0942116aa787402f433ebad3
Piotr Markielau <pmarkielau@gmail.com>
2024-04-05 19:25:44 +0200

    Add newsletter page
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- page-newsletter.php
+++ page-newsletter.php
@@ -8,34 +8,42 @@
          style="background-image: linear-gradient(135deg, rgba(229, 27, 77, 0.5), rgba(62, 2, 166, 0.5)),
                  url(<?php bloginfo( 'template_url' ); ?>/images/newsletter.jpg);">
         <p class="display-4 text-light text-shadow mt-3">Ты в клубе! Поздравляем 🎉</p>
-        <p class="lead my-4">Скоро ты получишь сообщение о добавлении в закрытую Google-группу. Чтобы получать более частые обновления от нас, подписывайся на нас в социальных сетях. </p>
-
-            <div class="container text-center py-3">
-                <a target="_blank" href="https://instagram.com/legalizebelarus"><img class="si2"
-                                                                                     src="<?php bloginfo( 'template_url' ); ?>/images/links/instagram.png"></a>
-
-                                <a target="_blank" href="https://t.me/LegalizeBelarus"><img class="si2"
-                                                                            src="<?php bloginfo( 'template_url' ); ?>/images/links/telegram.png"></a>
-
-                <a target="_blank" href="https://facebook.com/LegalizeBelarus"><img class="si2"
-                                                                                    src="<?php bloginfo( 'template_url' ); ?>/images/links/facebook.png"></a>
-                <a target="_blank" href="https://vk.com/legalizebelarus"><img class="si2"
-                                                                              src="<?php bloginfo( 'template_url' ); ?>/images/links/vk.png"></a>
-                <a target="_blank" href="https://www.youtube.com/@legalizebelarus"><img class="si2"
-                                                                                                        src="<?php bloginfo( 'template_url' ); ?>/images/links/youtube.png"></a>
-
-                <a target="_blank" href="https://ok.ru/legalizebelarus"><img class="si2"
-                                                                             src="<?php bloginfo( 'template_url' ); ?>/images/links/ok.png"></a>
-            </div>
-
-                   <p class="lead mt-4">На связи 😉</p>
-
 
     </div>
 </div>
 
 
+<div class="container cont bg-light pb-5 px-md-5">
 
+    <main role="main">
+        <section>
+            <div class="container pb-5 text-center">
+                <p class="lead my-4">Скоро ты получишь сообщение о добавлении в закрытую Google-группу. Чтобы получать более
+                    частые обновления от нас, подписывайся на нас в социальных сетях. </p>
+
+                <div class="container text-center py-3">
+                    <a target="_blank" href="https://instagram.com/legalizebelarus"><img class="si2"
+                                                                                         src="<?php bloginfo( 'template_url' ); ?>/images/links/instagram.png"></a>
+
+                    <a target="_blank" href="https://t.me/LegalizeBelarus"><img class="si2"
+                                                                                src="<?php bloginfo( 'template_url' ); ?>/images/links/telegram.png"></a>
+
+                    <a target="_blank" href="https://facebook.com/LegalizeBelarus"><img class="si2"
+                                                                                        src="<?php bloginfo( 'template_url' ); ?>/images/links/facebook.png"></a>
+                    <a target="_blank" href="https://vk.com/legalizebelarus"><img class="si2"
+                                                                                  src="<?php bloginfo( 'template_url' ); ?>/images/links/vk.png"></a>
+                    <a target="_blank" href="https://www.youtube.com/@legalizebelarus"><img class="si2"
+                                                                                            src="<?php bloginfo( 'template_url' ); ?>/images/links/youtube.png"></a>
+
+                    <a target="_blank" href="https://ok.ru/legalizebelarus"><img class="si2"
+                                                                                 src="<?php bloginfo( 'template_url' ); ?>/images/links/ok.png"></a>
+                </div>
+
+                <p class="lead mt-4">На связи 😉</p>
+            </div>
+        </section>
+    </main>
+</div>
 
 
 <?php get_footer(); ?>
