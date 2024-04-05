cdb60dd535efe94a755e2988a1830a50a5ef36da
Piotr Markielau <pmarkielau@gmail.com>
2024-04-05 19:02:36 +0200

    Update design
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- footer.php
+++ footer.php
@@ -1,19 +1,24 @@
 <footer>
     <div class="container px-md-5 pt-4">
         <div class="container text-center">
+                      <div class="container">
             <p class="text-light">Legalize Belarus © 2017–<?php echo date("Y"); ?></p>
             <a target="_blank" href="mailto:legalizebelarus.org@gmail.com">
                 <div class="btn btn-light mb-3">
                     🔗 <?php _e('Contact us', 'legalizebelarus'); ?>
                 </div>
             </a>
+            </div>
 
-            <form class="search mt-4 mb-5" method="get" action="<?php echo home_url(); ?>" role="search">
+              <div class="container mt-4 mb-5">
+                          <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
                 <input type="search" class="form-control"
                        placeholder="<?php _e('Search query', 'legalizebelarus'); ?>"
                        value="<?php echo get_search_query() ?>" name="s"
                        title="<?php echo esc_attr_x('Search for:', 'label') ?>"/>
             </form>
+              </div>
+
 
         </div>
     </div>
--- page.php
+++ page.php
@@ -21,6 +21,27 @@
 
 </div>
 
+	<div class="container">
+
+
+	<div class="tnp-subscription-posts bg-white" id="tnp-subscription-posts" style=""><div class="container text-center"><img alt="" width="128" height="128" data-src="/wp-content/themes/legalizebelarus/images/mushrooms.png" class="aligncenter wp-image-23265 size-full ls-is-cached lazyloaded" src="/wp-content/themes/legalizebelarus/images/mushrooms.png"><noscript><img class="aligncenter wp-image-23265 size-full" src="/wp-content/themes/legalizebelarus/images/mushrooms.png" alt="" width="128" height="128" /></noscript></div>
+<div>&nbsp;</div>
+<h5 style="text-align: center">Можно, если осторожно</h5>
+<p>Вступай в наш клуб за гуманную наркополитику. Твои данные не будут переданы третьим лицам.</p><div class="tnp tnp-subscription ">
+<form method="post" action="https://legalizebelarus.org/?na=s">
+<input type="hidden" name="nr" value="posts_bottom">
+<input type="hidden" name="nlang" value="ru">
+<div class="tnp-field tnp-field-email"><label for="tnp-1">Email</label>
+<input class="tnp-email" type="email" name="ne" id="tnp-1" value="" placeholder="" required=""></div>
+<div class="tnp-field tnp-privacy-field"><label><input type="checkbox" name="ny" required="" class="tnp-privacy"> Я даю согласие на использование переданного email-адреса для организации коммуникации с Legalize Belarus</label></div><div class="tnp-field tnp-field-button" style="text-align: left"><input class="tnp-submit" type="submit" value="Подписаться" style="">
+</div>
+</form>
+</div>
+	</div>
+
+
+		</div>
+
 
 <?php get_footer(); ?>
 
