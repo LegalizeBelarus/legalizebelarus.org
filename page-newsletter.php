cb03d8a5daae4c3dad377a0ba931de01c50744d6
Piotr Markielau <pmarkielau@gmail.com>
2024-04-05 19:17:56 +0200

    Add newsletter page
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- page-newsletter.php
+++ page-newsletter.php
@@ -3,13 +3,13 @@
 <?php get_header(); ?>
 
 
-<div class="container bg-light px-0 pb-4 pt-4 cont">
+<div class="container bg-light px-0 pt-4 cont">
     <div class="jumbotron d-flex flex-column jumbotron-fluid justify-content-center align-items-center px-3 text-center"
          style="background-image: linear-gradient(135deg, rgba(229, 27, 77, 0.5), rgba(62, 2, 166, 0.5)),
                  url(<?php bloginfo( 'template_url' ); ?>/images/newsletter.jpg);">
         <p class="display-4 text-light text-shadow mt-3">Ты в клубе! Поздравляем 🎉</p>
 
-            <div class="container text-center pb-3">
+            <div class="container text-center py-3">
                 <a target="_blank" href="https://facebook.com/LegalizeBelarus"><img class="si2"
                                                                                     src="<?php bloginfo( 'template_url' ); ?>/images/links/facebook.png"></a>
                 <a target="_blank" href="https://vk.com/legalizebelarus"><img class="si2"
@@ -26,12 +26,7 @@
 
                 <a target="_blank" href="https://www.patreon.com/LegalizeBelarus"><img class="si2"
                                                                                        src="<?php bloginfo( 'template_url' ); ?>/images/links/patreon.png"></a>
-                <a target="_blank" href="https://github.com/LegalizeBelarus"><img class="si2"
-                                                                                  src="<?php bloginfo( 'template_url' ); ?>/images/links/github.png"></a>
-                <a target="_blank" href="https://rarible.com/legalizebelarus"><img class="si2"
-                                                                                   src="<?php bloginfo( 'template_url' ); ?>/images/links/rarible.png"></a>
-                <a target="_blank" href="https://ru.wikipedia.org/wiki/Legalize_Belarus"><img class="si2"
-                                                                                   src="<?php bloginfo('template_url'); ?>/images/links/wiki.png"></a>
+
             </div>
 
     </div>
