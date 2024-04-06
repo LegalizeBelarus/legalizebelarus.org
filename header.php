c6d745536aa3063093d826934cbb592fdb34576a
Piotr Markielau <pmarkielau@gmail.com>
2024-04-06 14:16:40 +0200

    Add Meta pixel
    Signed-off-by: Piotr Markielau <pmarkielau@gmail.com>

--- header.php
+++ header.php
@@ -31,6 +31,24 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
 
     <?php wp_head(); ?>
+
+    <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1597342774167085');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1597342774167085&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
 </head>
 <body
         <?php body_class(); ?>>
